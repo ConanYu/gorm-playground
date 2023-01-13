64a72d325ea399d23682ebd9cda2615133dfe60a
ConanYu <C.zing@outlook.com>
2023-01-13 18:37:23 +0800

    bug report: go-gorm/gen with tag v0.3.19 will panic when use GenerateModel in Windows

--- main_test.go
+++ main_test.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"path"
+	"path/filepath"
+	"runtime"
 	"testing"
+
+	"gorm.io/gen"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -17,4 +23,18 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+}
+
+func TestGormGen(t *testing.T) {
+	_, filename, _, _ := runtime.Caller(0)
+	root := path.Dir(filename)
+	//modelPkgPath := path.Join(root, "model") // gorm.io/gen v0.3.19 Windows下会报错
+	modelPkgPath := filepath.ToSlash(path.Join(root, "model")) // gorm.io/gen v0.3.19 Windows下也会报错
+	fmt.Println(modelPkgPath)
+	g := gen.NewGenerator(gen.Config{
+		ModelPkgPath: modelPkgPath,
+	})
+	g.UseDB(DB)
+	g.GenerateModel("users")
+	g.Execute()
 }
